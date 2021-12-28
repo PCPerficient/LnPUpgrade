4607d2d4eb06da82e843d397b378670b748d9026
nilima.raut <nilima.raut@perficient.com>
2021-12-28 15:33:37 +0530

    Migrated the files reported by Gabriela in Excel.

---
+++ src/LeggettAndPlatt.Extensions/WebFramework/Templating/DotLiquidTags/HeaderScripts.cs
@@ -0,0 +1,33 @@
+﻿using DotLiquid;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeggettAndPlatt.Extensions.WebFramework.Templating.DotLiquidTags
+{
+   public class HeaderScripts:Tag
+    {
+
+        protected string settingName;
+        private string arg1 = string.Empty;
+        public override void Initialize(string tagName, string markup, List<string> tokens)
+        {
+            markup = markup.Trim();
+            this.settingName = LoadFontsInHeader();
+            base.Initialize(tagName, markup, tokens);
+        }
+
+        private string LoadFontsInHeader()
+        {
+            return $"<link href='https://fonts.googleapis.com/css?family=Roboto:300,300i,400,500,700,700i' rel='stylesheet'>";
+        }
+
+        public override void Render(Context context, TextWriter result)
+        {
+            result.Write(this.settingName);
+        }
+    }
+}
---
+++ src/LeggettAndPlatt.Extensions/WebFramework/Templating/DotLiquidTags/ProductDetailPageScript.cs
@@ -0,0 +1,37 @@
+﻿using DotLiquid;
+using Insite.Core.SystemSetting;
+using LeggettAndPlatt.Extensions.CustomSettings;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeggettAndPlatt.Extensions.WebFramework.Templating.DotLiquidTags
+{
+    public class ProductDetailPageScript:Tag
+    {
+        protected string settingName;
+        private string arg1 = string.Empty;
+        public override void Initialize(string tagName, string markup, List<string> tokens)
+        {
+            markup = markup.Trim();
+            this.settingName = LoadPDPScript();
+            base.Initialize(tagName, markup, tokens);
+        }
+
+        private string LoadPDPScript()
+        {
+
+            var addThisScriptURL = SettingsGroupProvider.Current.Get<SirvImagesSetting>().SirvScriptUrl;
+            return $"<script async = 'async' src = '{@addThisScriptURL}' type = 'text/javascript' ></script>";
+          
+        }
+
+        public override void Render(Context context, TextWriter result)
+        {
+            result.Write(this.settingName);
+        }
+    }
+}
